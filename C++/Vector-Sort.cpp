55f88c5e2a4579f40d8c54c6188069b2e7211342
Mrinal Pramanik <mrinalpramanik98@gmail.com>
2020-06-22 12:35:27 +0530

    C++ code

---
+++ C++/Vector-Sort.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+
+…}
+
+for(i=0;i<n;i++)
+{
+cout<<arr[i]<<" ";
+}
+
+return 0;
+
+}
